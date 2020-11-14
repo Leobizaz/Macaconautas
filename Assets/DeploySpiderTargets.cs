9de462a9f9b86fd6b53dd4aec28cf64ee6029822
Leonardo Biz Villar <leobizaz12@gmail.com>
2020-11-14 20:01:15 -0300

    Eta porra

---
+++ Assets/DeploySpiderTargets.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeploySpiderTargets : MonoBehaviour
+{
+    public GameObject[] targets;
+
+    private void Start()
+    {
+        foreach(GameObject target in targets)
+        {
+            target.transform.parent = null;
+        }
+    }
+}
