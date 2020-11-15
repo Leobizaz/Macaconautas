4924a94b1c4611168e5c0834b43d1da58a8526d6
Mjchez <51550417+Mjchez@users.noreply.github.com>
2020-11-15 15:28:08 -0300

    MenuDoCaralho

---
+++ Assets/Scripts/scrMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class scrMenu : MonoBehaviour
+{
+    public void QuitGame() {
+        Application.Quit();
+    }
+    public void GoMenu() {
+        SceneManager.LoadScene("Menu");
+    }
+    public void GoGame()
+    {
+        SceneManager.LoadScene("CenaPrincipal01");
+    }
+}
