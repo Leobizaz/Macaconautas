3bc6b4bda63a40e32fb268b26f4ff667b3c6b295
Henrikilis <73853874+Henrikilis@users.noreply.github.com>
2020-11-15 02:49:33 -0300

    scrapSpawn

--- Assets/scrapManager.cs
+++ Assets/scrapManager.cs
@@ -5,12 +5,32 @@ using UnityEngine.UI;
 
 public class scrapManager : MonoBehaviour
 {
+    public GameObject scrapPrefab;
+
     public float scrapTotal = 0;
     public Text textRef;
+    public int ScrapCount;
+
+    public Transform[] spawns;
+
+    public int numeberOfSeconds;
+    public int IsRuning = 1;
+
+    private float SpawnRangex;
+    private float SpawnRangez;
+    public float minSpawnx;
+    public float maxSpawnx;
+    public float minSpawnz;
+    public float maxSpawnz;
+
+    public Transform playerPosForSpawn;
 
     void Start()
     {
+        playerPosForSpawn = GameObject.FindGameObjectWithTag("Player").transform;
+        SimplePool.Preload(scrapPrefab, ScrapCount);
         textRef = GetComponentInChildren<Text>();
+
     }
 
    
@@ -23,6 +43,30 @@ public class scrapManager : MonoBehaviour
     {
         scrapTotal += value;
         textRef.text = scrapTotal.ToString();
+
+    }
+
+    IEnumerator spawnScrap()
+    {
+        IsRuning = 0;
+
+
+        for (int i = 0; i < ScrapCount; i++)
+        {
+
+            SpawnRangex = Random.Range(minSpawnx, maxSpawnx);
+            SpawnRangez = Random.Range(minSpawnz, maxSpawnz);
+            Vector3 randomPoint = new Vector3(playerPosForSpawn.position.x + SpawnRangex, 6, playerPosForSpawn.position.z + SpawnRangez);
+
+            GameObject spawnPoint = new GameObject();
+            spawnPoint.transform.position = randomPoint;
+            SimplePool.Spawn(scrapPrefab, spawnPoint.transform.position, Quaternion.identity);
+            Destroy(spawnPoint);
+        }
+
+
+        yield return new WaitForSeconds(numeberOfSeconds);
+        IsRuning = 1;
 
     }
 
