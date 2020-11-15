d376d8b5502d8645672d029bccec086ff0b196f2
Henrikilis <73853874+Henrikilis@users.noreply.github.com>
2020-11-15 04:37:52 -0300

    scrapstore

---
+++ Assets/destroyedTurretCollider.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class destroyedTurretCollider : MonoBehaviour
+{
+    public GameObject storeRef;
+    public float ScrapValue;
+    public bool hasBoght = false;
+    public int IsRuning = 1;
+    private void Start()
+    {
+        storeRef = GameObject.FindGameObjectWithTag("Scrapstore");
+    }
+
+    private void FixedUpdate()
+    {
+        if (IsRuning == 1)
+            StartCoroutine(check());
+
+
+        if (storeRef.activeInHierarchy)
+            GetComponent<BoxCollider>().gameObject.SetActive(false);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (!storeRef.activeInHierarchy && !hasBoght)
+                storeRef.SetActive(true);
+
+            SimplePool.Despawn(this.gameObject);
+        }
+    }
+
+     IEnumerator check()
+    {
+        IsRuning = 0;
+
+        yield return new WaitForSeconds(3);
+
+        if (!storeRef.activeInHierarchy)
+            GetComponent<BoxCollider>().gameObject.SetActive(true);
+
+        IsRuning = 1;
+    }
+}
---
+++ Assets/storeManager.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class storeManager : MonoBehaviour
+{
+    public GameObject dpsPrefab;
+    public GameObject tankPrefab;
+    public GameObject supportPrefab;
+
+    private destroyedTurretCollider destroyedRef;
+
+    public scrapManager scrapRef;
+
+    public GameObject coreRef;
+
+    public float CostTurret;
+
+    void Start()
+    {
+        scrapRef = FindObjectOfType<scrapManager>();
+        coreRef = GameObject.FindGameObjectWithTag("Core");
+        destroyedRef = FindObjectOfType<destroyedTurretCollider>();
+    }
+
+    public void DPSButton()
+    {
+        if(scrapRef.scrapTotal >= CostTurret)
+        {
+            Vector3 doladin = new Vector3(coreRef.transform.position.x + 2, coreRef.transform.position.y, coreRef.transform.position.z + 2);
+            var torre = Instantiate(dpsPrefab, coreRef.transform.position + doladin, Quaternion.identity);
+            torre.transform.parent = null;
+            GameEvents.current.CreateTower(torre);
+            destroyedRef.hasBoght = true;
+            this.gameObject.SetActive(false);
+        }
+
+    }
+
+    public void TankButton()
+    {
+        if (scrapRef.scrapTotal >= CostTurret)
+        {
+            Vector3 doladin = new Vector3(coreRef.transform.position.x + 2, coreRef.transform.position.y, coreRef.transform.position.z + 2);
+            var torre = Instantiate(tankPrefab, coreRef.transform.position + doladin, Quaternion.identity);
+            torre.transform.parent = null;
+            GameEvents.current.CreateTower(torre);
+            destroyedRef.hasBoght = true;
+            this.gameObject.SetActive(false);
+        }
+
+    }
+
+    public void SupportButton()
+    {
+        if (scrapRef.scrapTotal >= CostTurret)
+        {
+            Vector3 doladin = new Vector3(coreRef.transform.position.x + 2, coreRef.transform.position.y, coreRef.transform.position.z + 2);
+            var torre = Instantiate(supportPrefab, coreRef.transform.position + doladin, Quaternion.identity);
+            torre.transform.parent = null;
+            GameEvents.current.CreateTower(torre);
+            destroyedRef.hasBoght = true;
+            this.gameObject.SetActive(false);
+        }
+
+    }
+
+    public void ExitButton()
+    {
+        this.gameObject.SetActive(false);
+    }
+
+
+}
